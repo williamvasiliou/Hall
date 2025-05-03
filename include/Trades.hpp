c0be8d7b148dce0342102e21c3b18d9ba79c5719
williamvasiliou <195391054+williamvasiliou@users.noreply.github.com>
2025-05-03 23:59:28 +0000

    Add files via upload

--- include/Trades.hpp
+++ include/Trades.hpp
@@ -26,19 +26,24 @@ namespace Trades {
 		public:
 			static const Array *is(const JSON::Object *object, const std::string& name) {
 				if (object) {
-					const JSON::Array *array = object->get<JSON::Array>(name);
-					if (array) {
-						const size_t size = array->size();
-						std::vector<const T *> *vector = new std::vector<const T *>();
-						if (array->get<T, JSON::Object>(*vector) == size) {
-							return new Array(vector);
-						}
+					return Array<T>::is(object->get<JSON::Array>(name));
+				}
 
-						for (const auto& item : *vector) {
-							delete item;
-						}
-						delete vector;
+				return (Array *) nullptr;
+			}
+
+			static inline const Array *is(const JSON::Array *array) {
+				if (array) {
+					const size_t size = array->size();
+					std::vector<const T *> *vector = new std::vector<const T *>();
+					if (array->get<T, JSON::Object>(*vector) == size) {
+						return new Array(vector);
 					}
+
+					for (const auto& item : *vector) {
+						delete item;
+					}
+					delete vector;
 				}
 
 				return (Array *) nullptr;
@@ -85,13 +90,29 @@ namespace Trades {
 				if (object) {
 					const std::string *item = object->get<std::string, JSON::String, &JSON::String::string>("item");
 					if (item) {
+						const double *count = object->get<double, JSON::Number, &JSON::Number::number>("count");
 						const double *quantity = object->get<double, JSON::Number, &JSON::Number::number>("quantity");
-						if (quantity) {
-							return new Item(item, quantity);
-						}
-
-						return new Item(item, &Item::fallback);
+						return Item::is(item, count, quantity);
 					}
+
+					const std::string *tag = object->get<std::string, JSON::String, &JSON::String::string>("tag");
+					if (tag) {
+						return new Item(tag, &Item::fallback);
+					}
+				}
+
+				return (Item *) nullptr;
+			}
+
+			static const Item *is(const JSON::Object *object, const std::string *string) {
+				if (object) {
+					if (string) {
+						return (Item *) nullptr;
+					} else {
+						return Item::is(object);
+					}
+				} else if (string) {
+					return new Item(string, &Item::fallback);
 				}
 
 				return (Item *) nullptr;
@@ -109,6 +130,20 @@ namespace Trades {
 				item(item),
 				quantity(quantity)
 			{}
+
+			static inline const Item *is(const std::string *item, const double *count, const double *quantity) {
+				if (count) {
+					if (quantity) {
+						return (Item *) nullptr;
+					} else {
+						return new Item(item, count);
+					}
+				} else if (quantity) {
+					return new Item(item, quantity);
+				}
+
+				return new Item(item, &Item::fallback);
+			}
 	};
 
 	class Choice {
@@ -314,7 +349,7 @@ namespace Trades {
 
 	class Villager {
 		public:
-			static const Villager *is(const JSON::Object *object) {
+			static inline const Villager *is(const JSON::Object *object) {
 				const Array<Tier> *tiers = Array<Tier>::is(object, "tiers");
 				if (tiers) {
 					if (tiers->size() > 0) {
@@ -340,6 +375,577 @@ namespace Trades {
 		private:
 			explicit Villager(const Array<Tier> *tiers) :
 				tiers(tiers)
+			{}
+	};
+
+	template <size_t wants, size_t gives>
+	class Items {
+		public:
+			static const Items *is(const JSON::Object *object, const std::vector<std::string>& first, const std::vector<std::string>& second) {
+				if (object && first.size() == wants && second.size() == gives) {
+					std::vector<const Item *> *in = new std::vector<const Item *>();
+					std::vector<const Item *> *out = new std::vector<const Item *>();
+					for (const auto& name : first) {
+						const Item *item = Item::is(object->get<JSON::Object>(name), object->get<std::string, JSON::String, &JSON::String::string>(name));
+						if (item) {
+							in->push_back(item);
+						}
+					}
+
+					for (const auto& name : second) {
+						const Item *item = Item::is(object->get<JSON::Object>(name), object->get<std::string, JSON::String, &JSON::String::string>(name));
+						if (item) {
+							out->push_back(item);
+						}
+					}
+
+					if (in->size() == wants && out->size() == gives) {
+						return new Items(in, out);
+					}
+
+					for (const auto& item : *in) {
+						delete item;
+					}
+					delete in;
+
+					for (const auto& item : *out) {
+						delete item;
+					}
+					delete out;
+				}
+
+				return (Items *) nullptr;
+			}
+
+			const std::vector<const Item *> *in;
+			const std::vector<const Item *> *out;
+
+			template <typename T>
+			inline size_t push(T& trades) const noexcept {
+				AbstractTrade trade;
+
+				for (const auto& item : *this->in) {
+					trade.in[*item->item] += *item->quantity;
+				}
+
+				for (const auto& item : *this->out) {
+					trade.out[*item->item] += *item->quantity;
+				}
+
+				return trades.push(trade);
+			}
+
+			~Items() {
+				for (const auto& item : *this->in) {
+					delete item;
+				}
+				delete this->in;
+
+				for (const auto& item : *this->out) {
+					delete item;
+				}
+				delete this->out;
+			}
+
+		private:
+			explicit Items(const std::vector<const Item *> *in, const std::vector<const Item *> *out) :
+				in(in),
+				out(out)
+			{}
+	};
+
+	class Container {
+		public:
+			static const Container *is(const JSON::Object *object) {
+				const Items<2, 1> *items = Items<2, 1>::is(object, Container::first, Container::second);
+				if (items) {
+					return new Container(items);
+				}
+
+				return (Container *) nullptr;
+			}
+
+			const Items<2, 1> *items;
+
+			template <typename T>
+			inline size_t push(T& trades) const noexcept {
+				return this->items->push(trades);
+			}
+
+			~Container() {
+				delete this->items;
+			}
+		private:
+			explicit Container(const Items<2, 1> *items) :
+				items(items)
+			{}
+
+			static inline const std::vector<std::string> first{"input", "reagent"};
+			static inline const std::vector<std::string> second{"output"};
+	};
+
+	class Mix {
+		public:
+			static const Mix *is(const JSON::Object *object) {
+				const Items<2, 1> *items = Items<2, 1>::is(object, Mix::first, Mix::second);
+				if (items) {
+					return new Mix(items);
+				}
+
+				return (Mix *) nullptr;
+			}
+
+			const Items<2, 1> *items;
+
+			template <typename T>
+			inline size_t push(T& trades) const noexcept {
+				return this->items->push(trades);
+			}
+
+			~Mix() {
+				delete this->items;
+			}
+		private:
+			explicit Mix(const Items<2, 1> *items) :
+				items(items)
+			{}
+
+			static inline const std::vector<std::string> first{"input", "reagent"};
+			static inline const std::vector<std::string> second{"output"};
+	};
+
+	class Key {
+		public:
+			static const Key *is(const JSON::Object *object) {
+				if (object) {
+					size_t size = 0;
+					std::map<const char, const Item *> *items = new std::map<const char, const Item *>();
+					for (const std::pair<const std::string, JSON::Any *>& pair : object->object) {
+						++size;
+						if (pair.first.size() == 1) {
+							const Item *item = Item::is(JSON::Object::is(pair.second));
+							if (item) {
+								items->insert(std::pair<const char, const Item *>(pair.first[0], item));
+							}
+						}
+					}
+
+					if (size > 0) {
+						if (items->size() == size) {
+							return new Key(items);
+						}
+
+						for (const std::pair<const char, const Item *>& pair : *items) {
+							delete pair.second;
+						}
+					}
+					delete items;
+				}
+
+				return (Key *) nullptr;
+			}
+
+			const std::map<const char, const Item *> *items;
+
+			~Key() {
+				for (const std::pair<const char, const Item *>& pair : *this->items) {
+					delete pair.second;
+				}
+				delete this->items;
+			}
+
+		private:
+			explicit Key(const std::map<const char, const Item *> *items) :
+				items(items)
+			{}
+	};
+
+	class Result {
+		public:
+			static const Result *is(const JSON::Array *array, const JSON::Object *object) {
+				if (array) {
+					if (object) {
+						return (Result *) nullptr;
+					} else if (array->size() > 0) {
+						const Array<Item> *items = Array<Item>::is(array);
+						if (items) {
+							return new Result(items);
+						}
+					}
+				} else if (object) {
+					const Item *item = Item::is(object);
+					if (item) {
+						return new Result(Array<Item>::is(item));
+					}
+				}
+
+				return (Result *) nullptr;
+			}
+
+			const Array<Item> *items;
+
+			inline void push(AbstractTrade& trade) const noexcept {
+				const auto& items = *this->items;
+				const size_t size = items.size();
+				for (size_t i = 0; i < size; ++i) {
+					const Item *item = items[i];
+					trade.out[*item->item] += *item->quantity;
+				}
+			}
+
+			~Result() {
+				delete this->items;
+			}
+
+		private:
+			explicit Result(const Array<Item> *items) :
+				items(items)
+			{}
+	};
+
+	class Shaped {
+		public:
+			static const Shaped *is(const JSON::Object *object) {
+				if (object) {
+					const JSON::Array *pattern = object->get<JSON::Array>("pattern");
+					const Key *key = Key::is(object->get<JSON::Object>("key"));
+					const Result *result = Result::is(object->get<JSON::Array>("result"), object->get<JSON::Object>("result"));
+					if (pattern && key && result) {
+						const size_t size = pattern->size();
+						if (size > 0) {
+							std::vector<const std::string *> *vector = new std::vector<const std::string *>();
+							if (pattern->get<std::string, JSON::String, &JSON::String::string>(*vector) == size) {
+								return new Shaped(vector, key, result);
+							}
+							delete vector;
+						}
+						delete key;
+						delete result;
+					} else {
+						if (key) {
+							delete key;
+						}
+
+						if (result) {
+							delete result;
+						}
+					}
+				}
+
+				return (Shaped *) nullptr;
+			}
+
+			const std::vector<const std::string *> *pattern;
+			const Key *key;
+			const Result *result;
+
+			template <typename T>
+			inline size_t push(T& trades) const noexcept {
+				const auto& pattern = *this->pattern;
+				const auto& key = *this->key->items;
+				AbstractTrade trade;
+
+				for (const std::string *row : pattern) {
+					for (const char c : *row) {
+						const auto& pair = key.find(c);
+						if (pair != key.end()) {
+							const Item *item = pair->second;
+							trade.in[*item->item] += *item->quantity;
+						}
+					}
+				}
+
+				result->push(trade);
+				return trades.push(trade);
+			}
+
+			~Shaped() {
+				delete this->pattern;
+				delete this->key;
+				delete this->result;
+			}
+
+		private:
+			explicit Shaped(const std::vector<const std::string *> *pattern, const Key *key, const Result *result) :
+				pattern(pattern),
+				key(key),
+				result(result)
+			{}
+	};
+
+	class Shapeless {
+		public:
+			static const Shapeless *is(const JSON::Object *object) {
+				const Array<Item> *ingredients = Array<Item>::is(object, "ingredients");
+				if (ingredients) {
+					const Result *result = Result::is(object->get<JSON::Array>("result"), object->get<JSON::Object>("result"));
+					if (result) {
+						return new Shapeless(ingredients, result);
+					}
+					delete ingredients;
+				}
+
+				return (Shapeless *) nullptr;
+			}
+
+			const Array<Item> *ingredients;
+			const Result *result;
+
+			template <typename T>
+			inline size_t push(T& trades) const noexcept {
+				AbstractTrade trade;
+
+				const auto& ingredients = *this->ingredients;
+				const size_t size = ingredients.size();
+				for (size_t i = 0; i < size; ++i) {
+					const Item *item = ingredients[i];
+					trade.in[*item->item] += *item->quantity;
+				}
+
+				result->push(trade);
+				return trades.push(trade);
+			}
+
+			~Shapeless() {
+				delete this->ingredients;
+				delete this->result;
+			}
+
+		private:
+			explicit Shapeless(const Array<Item> *ingredients, const Result *result) :
+				ingredients(ingredients),
+				result(result)
+			{}
+	};
+
+	class Transform {
+		public:
+			static const Transform *is(const JSON::Object *object) {
+				const Items<2, 1> *items = Items<2, 1>::is(object, Transform::first, Transform::second);
+				if (items) {
+					return new Transform(items);
+				}
+
+				return (Transform *) nullptr;
+			}
+
+			const Items<2, 1> *items;
+
+			template <typename T>
+			inline size_t push(T& trades) const noexcept {
+				return this->items->push(trades);
+			}
+
+			~Transform() {
+				delete this->items;
+			}
+		private:
+			explicit Transform(const Items<2, 1> *items) :
+				items(items)
+			{}
+
+			static inline const std::vector<std::string> first{"base", "addition"};
+			static inline const std::vector<std::string> second{"result"};
+	};
+
+	class Trim {
+		public:
+			static const Trim *is(const JSON::Object *object) {
+				const Items<3, 1> *items = Items<3, 1>::is(object, Trim::first, Trim::second);
+				if (items) {
+					return new Trim(items);
+				}
+
+				return (Trim *) nullptr;
+			}
+
+			const Items<3, 1> *items;
+
+			template <typename T>
+			inline size_t push(T& trades) const noexcept {
+				return this->items->push(trades);
+			}
+
+			~Trim() {
+				delete this->items;
+			}
+		private:
+			explicit Trim(const Items<3, 1> *items) :
+				items(items)
+			{}
+
+			static inline const std::vector<std::string> first{"base", "addition", "template"};
+			static inline const std::vector<std::string> second{"base"};
+	};
+
+	template <typename T, typename U>
+	class Recipe {
+		public:
+			static inline const Recipe *is(const JSON::Object *object, const std::string& name, const std::string& key) {
+				if (object) {
+					const T *first = T::is(object->get<JSON::Object>(name));
+					const U *second = U::is(object->get<JSON::Object>(key));
+					if (first) {
+						if (second) {
+							delete first;
+							delete second;
+						} else {
+							return new Recipe(first);
+						}
+					} else if (second) {
+						return new Recipe(second);
+					}
+				}
+
+				return (Recipe *) nullptr;
+			}
+
+			const T *first;
+			const U *second;
+
+			template <typename V>
+			inline size_t push(V& trades) const noexcept {
+				if (this->first) {
+					return this->first->push(trades);
+				} else {
+					return this->second->push(trades);
+				}
+			}
+
+			~Recipe() {
+				if (this->first) {
+					delete this->first;
+				} else {
+					delete this->second;
+				}
+			}
+
+		private:
+			explicit Recipe(const T *first) :
+				first(first),
+				second((U *) nullptr)
+			{}
+
+			explicit Recipe(const U *second) :
+				first((T *) nullptr),
+				second(second)
+			{}
+	};
+
+	class Brewing {
+		public:
+			static inline const Brewing *is(const JSON::Object *object) {
+				const Recipe<Container, Mix> *recipe = Recipe<Container, Mix>::is(object, "minecraft:recipe_brewing_container", "minecraft:recipe_brewing_mix");
+				if (recipe) {
+					return new Brewing(recipe);
+				}
+
+				return (Brewing *) nullptr;
+			}
+
+			const Recipe<Container, Mix> *recipe;
+
+			template <typename T>
+			inline size_t push(T& trades) const noexcept {
+				return this->recipe->push(trades);
+			}
+
+			~Brewing() {
+				delete this->recipe;
+			}
+
+		private:
+			explicit Brewing(const Recipe<Container, Mix> *recipe) :
+				recipe(recipe)
+			{}
+	};
+
+	class Crafting {
+		public:
+			static inline const Crafting *is(const JSON::Object *object) {
+				const Recipe<Shaped, Shapeless> *recipe = Recipe<Shaped, Shapeless>::is(object, "minecraft:recipe_shaped", "minecraft:recipe_shapeless");
+				if (recipe) {
+					return new Crafting(recipe);
+				}
+
+				return (Crafting *) nullptr;
+			}
+
+			const Recipe<Shaped, Shapeless> *recipe;
+
+			template <typename T>
+			inline size_t push(T& trades) const noexcept {
+				return this->recipe->push(trades);
+			}
+
+			~Crafting() {
+				delete this->recipe;
+			}
+
+		private:
+			explicit Crafting(const Recipe<Shaped, Shapeless> *recipe) :
+				recipe(recipe)
+			{}
+	};
+
+	class Furnace {
+		public:
+			static inline const Furnace *is(const JSON::Object *object) {
+				if (object) {
+					const Items<1, 1> *items = Items<1, 1>::is(object->get<JSON::Object>("minecraft:recipe_furnace"), Furnace::first, Furnace::second);
+					if (items) {
+						return new Furnace(items);
+					}
+				}
+
+				return (Furnace *) nullptr;
+			}
+
+			const Items<1, 1> *items;
+
+			template <typename T>
+			inline size_t push(T& trades) const noexcept {
+				return this->items->push(trades);
+			}
+
+			~Furnace() {
+				delete this->items;
+			}
+
+		private:
+			explicit Furnace(const Items<1, 1> *items) :
+				items(items)
+			{}
+
+			static inline const std::vector<std::string> first{"input"};
+			static inline const std::vector<std::string> second{"output"};
+	};
+
+	class Smithing {
+		public:
+			static inline const Smithing *is(const JSON::Object *object) {
+				const Recipe<Transform, Trim> *recipe = Recipe<Transform, Trim>::is(object, "minecraft:recipe_smithing_transform", "minecraft:recipe_smithing_trim");
+				if (recipe) {
+					return new Smithing(recipe);
+				}
+
+				return (Smithing *) nullptr;
+			}
+
+			const Recipe<Transform, Trim> *recipe;
+
+			template <typename T>
+			inline size_t push(T& trades) const noexcept {
+				return this->recipe->push(trades);
+			}
+
+			~Smithing() {
+				delete this->recipe;
+			}
+
+		private:
+			explicit Smithing(const Recipe<Transform, Trim> *recipe) :
+				recipe(recipe)
 			{}
 	};
 
